961f06f2627453c924b59cac4d3d747d2e94e5de
akashraj1238 <akashrajjnv2002@gmail.com>
2024-09-24 19:41:36 +0530

    Pascal's Triangle

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,22 +1,14 @@
 def generate_pascals_triangle(numRows):
-    result = []  # Initialize the list to store rows of Pascal's triangle.
-
+    result = []  
     for i in range(numRows):
-        # Start each row with 1
-        currentRow = [1] * (i + 1)  # Every row starts and ends with 1
-        
-        # For elements between the first and last one, calculate their values
+        currentRow = [1] * (i + 1)  
         for j in range(1, i):
-            currentRow[j] = result[i-1][j-1] + result[i-1][j]  # Add values from the previous row
-            
-        # Append the current row to the result list
+            currentRow[j] = result[i-1][j-1] + result[i-1][j] 
         result.append(currentRow)
     
-    return result  # Return the list of rows
+    return result 
 
-# Example usage: Generate first 5 rows and print it as a string with each row on separate lines.
-triangle = generate_pascals_triangle(5)
-# print(triangle)
-# Pretty print each row of Pascal's Triangle
+triangle = int(input())
+
 for row in triangle:
     print(row)
